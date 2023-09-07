6c931bd501479bef5af6c133a5ec1826c6267ee5
Charlie Greenman <charlie@razroo.com>
2023-09-07 10:49:49 -0400

    Add code for angular-16.2.0 create-workspace

---
+++ apps/devgen-joe-frazier-demo/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'devgen-joe-frazier-demo-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'devgen-joe-frazier-demo';
+}
